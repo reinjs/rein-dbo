f39b656ecd74fb4951e22eadb90fd2d5f6ce6ea5
shenyunjie <shenyunjie@u51.com>
2018-05-22 21:48:31 +0800

    update

--- agent.js
+++ agent.js
@@ -4,7 +4,7 @@ const { formatter } = require('./utils');
 module.exports = async (app, plugin) => {
   const configs = formatter(plugin.config || {});
   const dbo = app.dbo = new YsDbo(configs);
-  
+  app.dbo.config = plugin.config || {};
   await dbo.connect();
   app.destroyed(async () => await dbo.disconnect());
 };
